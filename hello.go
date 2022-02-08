4041e1f851126263288eb0d613e87c3152bc5d39
ssumika <gsuzuki221@gmail.com>
2022-02-08 15:24:01 +0900

    Basics終わり

--- hello.go
+++ hello.go
@@ -11,6 +11,12 @@ import (
 //var 変数 型
 var i, j int = 1, 2
 
+//struct書き方
+type Vertex struct {
+	X int
+	Y int
+}
+
 //関数の基本的な使い方
 func add(x int, y int) int {
 	return x + y
@@ -31,12 +37,27 @@ func pow(x, n, lim float64) float64 {
 	return lim
 }
 
-func main() {
-	defer fmt.Println("deferは呼び出し元の関数がretrunするまで実行されない(関数の引数はすぐ評価される)")
-	for i := 0; i < 10; i++ {
-		//deferはLIFO
-		defer fmt.Println(i)
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
 	}
+}
+
+func main() {
+	defer fmt.Println("deferは呼び出し元の関数がretrunするまで実行されない(関数の引数はすぐ評価される),LIFO方式")
+	/*
+		for i := 0; i < 10; i++ {
+			//deferはLIFO
+			defer fmt.Println(i)
+		}
+	*/
 
 	fmt.Println(add(42, 13))
 	//暗黙的な宣言(関数外では使用不可)
@@ -69,4 +90,62 @@ func main() {
 	default:
 		fmt.Println("Too far away.")
 	}
+
+	//pointer,structの宣言
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
+	//v2=Vertex{X:1}やVertex{}など一部だけの初期化も可能
+
+	//配列使い方
+	var ary [2]string
+	ary[0] = "Hello"
+	ary[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
+	//スライスも使える,スライスを変更するともとの配列に変更が適用される
+	var s []int = primes[1:4]
+	fmt.Println(s)
+
+	//make関数:動的サイズの配列作成
+	sa := make([]int, 5)
+	printSlice("a", sa)
+
+	sb := make([]int, 0, 5)
+	printSlice("b", sb)
+	//以下のようにappend可能(容量が小さくてもOK)
+	sb = append(sb, 0)
+	printSlice("sb", sb)
+
+	//for i,v :=range pow{}のように使うとi:index,v:valueが返される(for _,vのように捨てることも可能)
+
+	//map型も使用可能
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	val, ok := m["Answer"]
+	fmt.Println("The value:", val, "Present?", ok)
+
+	//Goの関数はクロジャー(自身の外部から変数を参照する関数値)
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+
 }

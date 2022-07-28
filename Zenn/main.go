81a40d826c93092cc06d739d145bab70871fe10d
ssumika <gsuzuki221@gmail.com>
2022-07-28 13:58:07 +0900

    https://zenn.dev/ichi320/books/0f544e3a076ba2b7212f/viewer/d7375aまで学習

--- Zenn/main.go
+++ Zenn/main.go
@@ -1,79 +1,32 @@
 package main
 
 import (
-	"log"
+	"html/template"
 	"os"
-	"strings"
-	"text/template"
+	"time"
 )
 
 var tpl *template.Template
 
-type dog struct {
-	Kind    string
-	Country string
-}
-
-type vegetable struct {
-	Name  string
-	Color string
-}
-
-var funcmap = template.FuncMap{
-	"upper": strings.ToUpper,
-	"three": myThree,
-}
-
 func init() {
-	tpl = template.Must(template.New("").Funcs(funcmap).ParseFiles("tpl.gohtml"))
-}
-
-func myThree(s string) string {
-	s = strings.TrimSpace(s)
-	if len(s) > 3 {
-		s = s[:3]
-	}
-	return s
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
 func main() {
 
-	cute := dog{
-		Kind:    "Samoyed",
-		Country: "Russia",
-	}
-	cute2 := dog{
-		Kind:    "Akita",
-		Country: "Japan",
+	type source struct {
+		Title, Message string
+		Time           time.Time
 	}
 
-	v1 := vegetable{
-		Name:  "onion",
-		Color: "white",
-	}
-	v2 := vegetable{
-		Name:  "carrot",
-		Color: "orange",
-	}
-	dogs := []dog{cute, cute2}
-	vegetables := []vegetable{v1, v2}
-
-	data := struct {
-		AAA []dog
-		BBB []vegetable
-	}{
-		dogs,
-		vegetables,
-	}
-
-	nf, err := os.Create("index.html")
-	if err != nil {
-		log.Fatalln("error creating file", err)
-	}
+	nf, _ := os.Create("index.html")
 	defer nf.Close()
 
-	err = tpl.ExecuteTemplate(nf, "tpl.gohtml", data)
-	if err != nil {
-		log.Fatalln(err)
+	var data source
+	data = source{
+		Title:   "test page",
+		Message: "本日は晴天なり",
+		Time:    time.Now(),
 	}
+	_ = tpl.ExecuteTemplate(nf, "tpl.gohtml", data)
 }

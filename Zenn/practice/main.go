c993baa81c3244ff2fb03d357210539454047872
ssumika <gsuzuki221@gmail.com>
2022-08-17 16:46:56 +0900

    https://zenn.dev/ichi320/books/0f544e3a076ba2b7212f/viewer/64860f　まで

--- Zenn/practice/main.go
+++ Zenn/practice/main.go
@@ -28,6 +28,8 @@ func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/vip", vip)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe("localhost:8080", nil)
 }
@@ -89,4 +91,63 @@ func signup(w http.ResponseWriter, req *http.Request) {
 	}
 
 	tpl.ExecuteTemplate(w, "signup.html", nil)
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	// ログイン済みの場合はこのページは不必要
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// 情報の送信を受け取ったときの処理
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+
+		// ユーザーDBから情報を取ってくる
+		u, ok := dbUsers[un]
+		// そもそもDBになかったら失敗
+		if !ok {
+			http.Error(w, "USER NAME - PASSWORD not match", http.StatusForbidden)
+			return
+		}
+		// 受信したパスワードとDBのパスワードの照合
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(w, "USER NAME - PASSWORD not match", http.StatusForbidden)
+			return
+		}
+
+		// セッションを作成する
+		sID := uuid.New()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		http.Redirect(w, req, "/", http.StatusSeeOther) // 303
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "login.html", nil)
+}
+
+func logout(w http.ResponseWriter, req *http.Request) {
+	if !alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther) // 303
+		return
+	}
+	c, _ := req.Cookie("session")
+	// セッションから抜ける（mapからキーを削除する）
+	delete(dbSessions, c.Value)
+	// remove the cookie
+	c = &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, c)
+	http.Redirect(w, req, "/login", http.StatusSeeOther) // 303
 }
